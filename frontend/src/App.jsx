38cbeff3d11a5d217560ff3c280c1e0c0df293eb
tgldrchn <tuguldur9190@gmail.com>
2023-02-16 18:59:38 +0800

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
