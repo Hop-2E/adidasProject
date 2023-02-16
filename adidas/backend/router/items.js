a329c05b3cb9296d036456c9a57caa04f449fb1a
tgldrchn <tuguldur9190@gmail.com>
2023-02-16 19:16:55 +0800

    commit

--- adidas/backend/router/items.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getAllItems, createItem, deleteItem } from "../controller/items.js";
-const router = express.Router();
-
-router.route("/").get(getAllItems).post(createItem).delete(deleteItem);
-export default router;
