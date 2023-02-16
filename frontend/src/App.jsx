36eb5554f814f51386a6ced0c42df61450b7a4ab
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-02-16 18:22:29 +0800

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/header"
+import Home from './pages/home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/'  element = {<Home />}/>
         <Route path='/header' element = {<Header/>}/>
       </Routes>
     </BrowserRouter>
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,35 +1,59 @@
 import Logo from "../assets/adidasLogo.png"
+import { VscAccount } from "react-icons/vsc";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiShoppingBag , BiSearch } from "react-icons/bi";
+import { RxCaretDown } from "react-icons/rx";
+import { Link } from "react-router-dom";
 import "../App.css"
 
 const Header = () =>{
 
-    return (
-       <div className="header">
-            <div className="logo">
-              <img src={Logo} alt="" />
+    return (<>
+    <div className="headerZero">
+        <h4>GET A $20 BONUS REWARD</h4>
+        <h4>FREE STANDARD SHIPPING & RETURNS</h4>
+        <h4>FRESH SALE STYLES: UP TO 50% OFF</h4>
+        <RxCaretDown />
+    </div>
+    <div className="header">
+            <div>
+                <Link to = {"/"}>
+                    <img src={Logo} alt="" className="logo"/>
+                </Link>
             </div>
             <div className="mainHeader">
-                <div>men </div>
-                <div>women </div>
-                <div>kids </div>
-                <div>sale </div>
-                <div>3 stripe life</div>
+                <div>MEN </div>
+                <div>WOMEN </div>
+                <div>KIDS </div>
+                <div>SALE </div>
+                <div>3 SRTRIPE LIFE</div>
             </div>
             <div className="rightHeader">
                 <div className="rightHeaderDiv">
                     <div>help</div>
-                    <div>Exchanges & Returns</div>
-                    <div>Order Tracker</div>
-                    <div>join adiClub</div>
+                    <div>exchanges & returns</div>
+                    <div>order tracker</div>
+                    <div>join adiclub</div>
                 </div>
                 <div className="rightHeaderDiv">
-                    <div>search</div>
-                    <div>pro</div>
-                    <div>like</div>
-                    <div>shop</div>
+                    <div style={{ border: '1px solid black', display:'flex', alignItems:'center'}}>
+                        <input type="search" name="" id=""  placeholder="Search" style={{ border:'none'}} />
+                        <BiSearch />
+                    </div>
+                    <div>
+                     <VscAccount/>
+                    </div>
+                    <div>
+                        <AiOutlineHeart />
+                    </div>
+                    <div>
+                        <BiShoppingBag/>
+                    </div>
                 </div>
             </div>
        </div>
+    </>
+       
     )
 }
 export default Header;
