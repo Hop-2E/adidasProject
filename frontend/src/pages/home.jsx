51a0d20250781afd263494996c1d8cf883b3f026
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-02-18 19:20:30 +0800

    body

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import Body from "../components/body";
 
 const Home = () => {
   return (
     <div className="homeContainer">
       <Header />
+      <Body />
     </div>
   );
 };
