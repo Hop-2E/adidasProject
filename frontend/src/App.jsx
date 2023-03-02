fee4e9652721c9b12c4ff6ef20950c21c5c800f1
UskhuuU <110882884+UskhuuU@users.noreply.github.com>
2023-03-02 17:10:53 +0800

    Revert "men shoes"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/header";
 import Home from "./pages/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/header" element={<Header />} />
-          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </>
