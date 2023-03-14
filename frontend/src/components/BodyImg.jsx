f9eb19bbac8bbeaebf0094203dbcb4f6ce1bdfd7
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-03-14 19:22:07 +0800

    bodybtn

--- frontend/src/components/BodyImg.jsx
+++ frontend/src/components/BodyImg.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
+import BodyBtn from "./bodybtn";
 const BodyImg = () => {
   return (
     <Carousel>
@@ -9,6 +10,7 @@ const BodyImg = () => {
           src="https://approval.studio/wp-content/uploads/elementor/thumbs/branding-story-of-adidas-phxr316ifopepwoi0z20kyaqn4g405p30ulyt2m8fk.jpg"
           alt="First slide"
         />
+        <BodyBtn />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
@@ -17,6 +19,7 @@ const BodyImg = () => {
           src="https://i.ytimg.com/vi/FwR2iVYfDV4/maxresdefault.jpg"
           alt="Second slide"
         />
+        <BodyBtn />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
@@ -25,6 +28,7 @@ const BodyImg = () => {
           src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/84589835172167.56ec20e07a472.png"
           alt="Third slide"
         />
+        <BodyBtn />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
