ae3539414226eb602263133cd4a2294a972a837d
munkhOrgilKing <110896103+munkhorgi@users.noreply.github.com>
2023-03-16 16:19:54 +0800

    80 homebutton (#83)
    * bodybtn
    
    * a
    
    ---------
    
    Co-authored-by: 22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>

---
+++ frontend/src/components/bodybtn.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+const BodyBtn = () => {
+  return (
+    <div className="bodybtn">
+      <h1>STAN SMITH</h1>
+      <div>
+        Reborn for a generation that refuses to play by the rules. Welcome to a
+        new era.
+      </div>
+      <div className="btn">
+        <Button variant="light" size="lg" className="mb-3">
+          <Link to={"/men"}>
+            SHOP MEN{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </Link>
+        </Button>
+        <Button variant="light" size="lg">
+          <Link to={"/women"}>
+            SHOP WOMEN{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default BodyBtn;
