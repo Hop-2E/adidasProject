4e07cc5cade88c278f94e104c22e7658e8078cd8
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-03-16 16:37:01 +0800

    New_Button

--- frontend/src/components/bodybtn.jsx
+++ frontend/src/components/bodybtn.jsx
@@ -10,7 +10,7 @@ const BodyBtn = () => {
         new era.
       </div>
       <div className="btn">
-        <Button variant="light" size="lg" className="mb-3">
+        <Button variant="outline-light" size="lg" className="mb-3">
           <Link to={"/men"}>
             SHOP MEN{" "}
             <svg
@@ -28,9 +28,27 @@ const BodyBtn = () => {
             </svg>
           </Link>
         </Button>
-        <Button variant="light" size="lg">
+        <Button variant="outline-light" size="lg" className="mb-3">
           <Link to={"/women"}>
             SHOP WOMEN{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </Link>
+        </Button>
+        <Button variant="outline-light">
+        <Link to={"/kids"}>
+            SHOP KIDS{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
